67d47ba0a9cfb1f0773d5f95f8560e8dc995d60d
Valerii Inshyn <valeriii@callcabinet.com>
2023-12-07 18:37:27 +0200

    fix: deleted wrong test

--- sombra-app/src/app/app.component.spec.ts
+++ sombra-app/src/app/app.component.spec.ts
@@ -14,11 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'sombra-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sombra-app');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
